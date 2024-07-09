91d8fb0211b941e66a38ea17b3f9a27dcbb2bfa7
L0rdLizard <WarriorXKZ@gmail.com>
2024-07-09 13:53:32 +0300

    complete

--- task330/task330.cpp
+++ task330/task330.cpp
@@ -3,7 +3,7 @@
 class Solution {
 public:
     int minPatches(std::vector<int>& nums, int n) {
-        int result = 0;
+        short result = 0;
         long long sum = 0;
         nums.push_back(n);
         if(nums[0] != 1){
@@ -11,15 +11,36 @@ public:
             result++;
         }
         
-        for (int i = 0; i < nums.size() - 1; i++) {
+        for (short i = 0; i < nums.size() - 1; i++) {
             sum+=nums[i];
-            
+            if (sum > n) return result;
             if ( (nums[i + 1] - sum) > 1 ){
                 result++;
                 nums.insert(nums.begin() + i + 1, sum + 1);
             }
-            if (nums[i + 1] > n) return result;
+            
         }
+        return result;
+    }
+};
+
+class Solution {
+public:
+    int minPatches(std::vector<int>& nums, int n) {
+        int result = 0;
+        long long sum = 0;
+        int i = 0;
+
+        while (sum < n) {
+            if (i < nums.size() && nums[i] <= sum + 1) {
+                sum += nums[i];
+                i++;
+            } else {
+                sum += sum + 1;
+                result++;
+            }
+        }
+
         return result;
     }
 };
@@ -27,8 +48,8 @@ public:
 int main(int argc, char const *argv[])
 {
     Solution s;
-    std::vector<int> nums = {1, 5, 10};
-    int n = 20;
+    std::vector<int> nums = {1,7,21,31,34,37,40,43,49,87,90,92,93,98,99};
+    int n = 12;
     int result = s.minPatches(nums, n);
     std::cout << result << std::endl;
     return 0;
