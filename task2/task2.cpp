68211954bf45da4d886e0e636053774901384dcd
LordLizard Notebook <WarriorXKZ@gmail.com>
2024-08-12 23:49:09 +0300

    task2 complete

--- task2/task2.cpp
+++ task2/task2.cpp
@@ -1,4 +1,5 @@
-
+#include <iostream>
+using namespace std;
 
 struct ListNode {
     int val;
@@ -11,7 +12,29 @@ struct ListNode {
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
+        ListNode* result = new ListNode(0);
+        ListNode* curResult = result;
+        int carry = 0;
+
+        while (l1 != nullptr || l2 != nullptr) {
+            int val1 = l1 ? l1->val : 0;
+            int val2 = l2 ? l2->val : 0;
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+            curResult->next = new ListNode(sum % 10);
+            curResult = curResult->next;
+
+            if (l1) l1 = l1->next;
+            if (l2) l2 = l2->next;
+        }
+
+        if (carry > 0) {
+            curResult->next = new ListNode(carry);
+        }
+
+        ListNode* head = result->next;
+        delete result;
+        return head;
     }
 };
 
@@ -22,7 +45,15 @@ int main(int argc, char const *argv[])
     ListNode l1 = ListNode(2, &l12);
 
     ListNode l23 = ListNode(4);
-    ListNode l22 = ListNode(6, &l13);
-    ListNode l2 = ListNode(5, &l12);
+    ListNode l22 = ListNode(6, &l23);
+    ListNode l2 = ListNode(5, &l22);
+
+    cout << l1.val;
+    cout << l1.next->val;
+    cout << l1.next->next->val;
+
+    cout << endl;
+    Solution solution = Solution();
+    solution.addTwoNumbers(&l1, &l2)->val;
     return 0;
 }
