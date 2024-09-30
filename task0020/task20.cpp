cad519d8caa2e2636ad17180cea8ca20d19b948b
L0rdLizard <WarriorXKZ@gmail.com>
2024-09-30 19:13:32 +0300

    task20

---
+++ task0020/task20.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stack;
+        for (char c : s) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.empty()) {
+                    return false;
+                }
+                if (c == ')' && stack.top() != '(') {
+                    return false;
+                }
+                if (c == ']' && stack.top() != '[') {
+                    return false;
+                }
+                if (c == '}' && stack.top() != '{') {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.empty();
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
